26786626cfd5c2525d2d49fae5750d791c522fdc
Bryce L Taylor <bryceLTaylor@gmail.com>
2022-07-11 12:37:03 -0400

    setup webpack

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import webpack from 'webpack';
+import * as webpackConfig from '../webpack.config.js';
+import webpackDevMiddleware from 'webpack-dev-middleware';
 
 const hostName = 'localHost';
 const port = '8002';
 
+const compiler = webpack(webpackConfig.default);
+let middlewareOptions = {};
+
 let app = express();
 
-app.use(express.static('./src'));
+app.use(webpackDevMiddleware(compiler, middlewareOptions));
 
 let server = app.listen(port, () => {
   console.log(`express app listening at ${hostName}:${port}`);
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+import path from 'path';
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+    mode: 'development',
+    entry: path.join(__dirname, "src", "index.js"),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'warlockCreator.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.?jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "index.html")
+        }),
+    ]
+};
