7e3fb05f1ec33fbbff630d4f79cf3fc2ce327769
Bryce L Taylor <bryceLTaylor@gmail.com>
2022-07-11 12:38:33 -0400

    setup react

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
+import Page from './components/page.jsx';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<Page />);
