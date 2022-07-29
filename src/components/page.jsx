85d535d6bbb02a192d707527272a047449f81d99
Bryce L Taylor <bryceLTaylor@gmail.com>
2022-07-29 16:20:03 -0400

    Clean up page component

---
+++ src/components/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./header.jsx";
+import Creator from './Creator.js';
+
+const Page = (props) => (
+  <div>
+    <Header />
+    <Creator />
+  </div>
+)
+
+export default Page;
--- src/components/page/page.jsx
+++ src/components/page/page.jsx
@@ -3,7 +3,9 @@ import Header from "../header/Header.jsx";
 import Creator from '../creator/Creator.js';
 
 const Page = (props) => (
-  <div>
+  <div
+    className = 'page'
+  >
     <Header />
     <Creator />
   </div>
